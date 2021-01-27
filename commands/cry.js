4bd21fc8061aafc527eefafadb469e9bfb453b4b
JustBorysov <63719176+JustBorysov@users.noreply.github.com>
2021-01-27 21:59:40 +0200

    Create cry.js

---
+++ commands/cry.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+
+module.exports = {
+    name: 'baka',
+    execute(client, message, args){
+        if (!message.guild) return;
+            async function cry() {
+            const GIF = await neko.sfw.cry();
+            const embed = new Discord.MessageEmbed()
+            .setColor('#202225')
+            .setTitle(`${message.author.tag} вот рандомное изображение/гиф бака`)
+            .setImage(GIF.url)
+            message.channel.send(embed);
+            }
+            cry();
+    }
+}
