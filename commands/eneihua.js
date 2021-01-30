cf69e78638575ef6cccfa1644c61262f18bcca8a
JustBorysov <63719176+JustBorysov@users.noreply.github.com>
2021-01-30 23:41:26 +0200

    Create eneihua.js

---
+++ commands/eneihua.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'eneihua',
+    execute(client, message, args){
+        if (!message.guild) return;
+            const embed = new Discord.MessageEmbed()
+            .setColor('#202225')
+            .setTitle(`ПУК ПУК`)
+            message.channel.send(embed);
+            }
+    }
