447065952f02f6815bd29e7949d285eb91158f3a
JustBorysov <63719176+JustBorysov@users.noreply.github.com>
2021-03-04 15:11:37 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -12,7 +12,7 @@ module.exports = {
                 const embed = new Discord.MessageEmbed()
                 .setColor('#202225')
                 .setTitle(`Тест!`)
-                .setFooter(`@<${message.author.id}> всё будет хорошо, солнышко!`)
+                .setField(`@<${message.author.id}> всё будет хорошо, солнышко!`)
                 .setImage(GIF.url)
                 message.channel.send(embed);
             }
