a0967cf7bb4ffceff5f109171c9b3894b18726e4
JustBorysov <63719176+JustBorysov@users.noreply.github.com>
2021-03-04 15:09:01 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -12,7 +12,7 @@ module.exports = {
                 const embed = new Discord.MessageEmbed()
                 .setColor('#202225')
                 .setTitle(`Тест!`)
-                .setDescription(`${message.author.tag} всё будет хорошо, солнышко!`)
+                .setDescription(`@<${message.author.id}> всё будет хорошо, солнышко!`)
                 .setImage(GIF.url)
                 message.channel.send(embed);
             }
@@ -25,7 +25,7 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
             .setColor('#202225')
             .setTitle(`Тест!`)
-            .setDescription(`<@${member.user.id}> обнял(а) @<${member.user.id}>`)
+            .setDescription(`<@${member.author.id}> обнял(а) @<${member.user.id}>`)
             .setImage(GIF.url)
             message.channel.send(embed);
             }
