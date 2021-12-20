0c96110735c2bc49d1173158f2df4fdd91a29975
Artem Borysov <68289012+Qyymmu@users.noreply.github.com>
2021-12-20 15:33:19 +0200

    Update neko.js

--- commands/neko.js
+++ commands/neko.js
@@ -10,7 +10,7 @@ module.exports = {
             const GIF = await neko.sfw.neko();
             const embed = new Discord.MessageEmbed()
             .setColor('#202225')
-            .setTitle(`${message.author.id} Ня!`)
+            .setTitle(`<@${message.author.id}> Ня!`)
             .setImage(GIF.url)
             message.channel.send(embed);
             }
